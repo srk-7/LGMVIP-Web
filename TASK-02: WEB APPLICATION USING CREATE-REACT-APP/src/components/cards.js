4481f993399c4b8329ece0ff86119818a3bc5a53
ARAVAPALLY SHIVARAMAKRISHNA <105867469+srk-7@users.noreply.github.com>
2022-11-23 19:48:52 +0530

    Add files via upload

--- TASK-02: WEB APPLICATION USING CREATE-REACT-APP/src/components/cards.js
+++ TASK-02: WEB APPLICATION USING CREATE-REACT-APP/src/components/cards.js
@@ -1,1 +1,33 @@
+import React from "react";
 
+const Users = ({ loading, users }) => {
+  const mainstyle = {
+    display: "flex"
+  };
+  return loading ? (
+    <div id="main" align="center">
+      <img
+        src="https://acegif.com/wp-content/uploads/loading-45.gif"
+        alt="Loaading.."
+        className="loader"
+      />
+    </div>
+  ) : (
+    <div id="main" style={mainstyle}>
+      {users.map((user) => (
+        <div className="profile_cont" key={user.id}>
+          {console.log(user)}
+
+          <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+          <h1 className="name">
+            {user.first_name} {user.last_name}
+          </h1>
+          <p className="email">{user.email}</p>
+          <p>User ID: {user.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
